33ec078cebe98f53088ff39c01bbeb15b1ebf529
Ben Adams <thundercat@illyriad.co.uk>
2019-06-30 15:16:55 +0100

    Host.Context pooling

--- src/Hosting/Hosting/src/Internal/HostingApplication.cs
+++ src/Hosting/Hosting/src/Internal/HostingApplication.cs
@@ -5,13 +5,14 @@ using System;
 using System.Diagnostics;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting.Server;
+using Microsoft.AspNetCore.Hosting.Server.Abstractions;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.Features;
 using Microsoft.Extensions.Logging;
 
 namespace Microsoft.AspNetCore.Hosting
 {
-    internal class HostingApplication : IHttpApplication<HostingApplication.Context>
+    internal class HostingApplication : IHttpApplication<HostingApplication.ContextWrapper>
     {
         private readonly RequestDelegate _application;
         private readonly IHttpContextFactory _httpContextFactory;
@@ -29,40 +30,85 @@ namespace Microsoft.AspNetCore.Hosting
         }
 
         // Set up the request
-        public Context CreateContext(IFeatureCollection contextFeatures)
+        public ContextWrapper CreateContext(IFeatureCollection contextFeatures)
         {
-            var context = new Context();
             var httpContext = _httpContextFactory.Create(contextFeatures);
 
-            _diagnostics.BeginRequest(httpContext, ref context);
+            Context hostContext;
+            if (contextFeatures is IHostContextContainer<ContextWrapper> container)
+            {
+                hostContext = container.HostContext.Context;
+                if (hostContext is null)
+                {
+                    hostContext = new Context();
+                    // Initalize the wrapper struct in-place; so its wrapped object reference gets set
+                    container.HostContext = new ContextWrapper(hostContext);
+                }
+            }
+            else
+            {
+                // Server doesn't support pooling, so create a new Context
+                hostContext = new Context();
+            }
 
-            context.HttpContext = httpContext;
-            return context;
+            hostContext.HttpContext = httpContext;
+            _diagnostics.BeginRequest(httpContext, hostContext);
+            return new ContextWrapper(hostContext);
         }
 
         // Execute the request
-        public Task ProcessRequestAsync(Context context)
+        public Task ProcessRequestAsync(ContextWrapper contextWrapper)
         {
-            return _application(context.HttpContext);
+            return _application(contextWrapper.Context.HttpContext);
         }
 
         // Clean up the request
-        public void DisposeContext(Context context, Exception exception)
+        public void DisposeContext(ContextWrapper contextWrapper, Exception exception)
         {
+            var context = contextWrapper.Context;
             var httpContext = context.HttpContext;
             _diagnostics.RequestEnd(httpContext, exception, context);
             _httpContextFactory.Dispose(httpContext);
             _diagnostics.ContextDisposed(context);
+
+            // Reset the context as it may be pooled
+            context.Reset();
         }
 
-        internal struct Context
+
+        internal class Context
         {
             public HttpContext HttpContext { get; set; }
             public IDisposable Scope { get; set; }
-            public long StartTimestamp { get; set; }
-            public bool EventLogEnabled { get; set; }
             public Activity Activity { get; set; }
+
+            public long StartTimestamp { get; set; }
             internal bool HasDiagnosticListener { get; set; }
+            public bool EventLogEnabled { get; set; }
+
+            public void Reset()
+            {
+                HttpContext = null;
+                Scope = null;
+                Activity = null;
+
+                StartTimestamp = 0;
+                HasDiagnosticListener = false;
+                EventLogEnabled = false;
+            }
+        }
+
+        /// <summary>
+        /// Struct wrapper to devirtualize <see cref="IHttpApplication{TContext}"/> into faster concrete generic implementations rather than shared generics.
+        /// </summary>
+        internal readonly struct ContextWrapper
+        {
+            public ContextWrapper(Context context)
+            {
+                Context = context;
+            }
+
+            public Context Context { get; }
         }
     }
 }
